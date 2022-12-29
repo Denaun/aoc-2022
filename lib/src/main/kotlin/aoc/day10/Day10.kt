0f7ef6ac1a218819b680b279a6dc06bc10fa2e93
Maurizio Zucchelli <mauzuc90@yahoo.it>
2022-12-29 14:00:46 +0100

    Implement day 10, part 2.

--- lib/src/main/kotlin/aoc/day10/Day10.kt
+++ lib/src/main/kotlin/aoc/day10/Day10.kt
@@ -6,6 +6,7 @@ fun part1(input: String): Int = execute(parse(input))
     .windowed(1, 40)
     .sumOf { (x) -> x.value * x.index }
 
+fun part2(input: String): String = execute(parse(input)).draw()
 
 fun execute(instructions: List<Instruction>): Sequence<Int> = sequence {
     var x = 1
@@ -19,4 +20,24 @@ fun execute(instructions: List<Instruction>): Sequence<Int> = sequence {
         }
     }
     yield(x)
+}
+
+fun Sequence<Int>.draw(): String {
+    val litPixels = List(6) { MutableList(40) { false } }
+    for ((cycle, offset) in drop(1).withIndex()) {
+        val column = cycle % 40
+        val row = (cycle / 40) % 6
+        if (offset - 1 <= column && column <= offset + 1) {
+            litPixels[row][column] = true
+        }
+    }
+    return litPixels.joinToString("\n") {
+        it.joinToString("") { isLit ->
+            if (isLit) {
+                "#"
+            } else {
+                "."
+            }
+        }
+    }
 }
